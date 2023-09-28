dabffa8b5e6bf44d2d36679a1674d253bdcde155
mammadov115 <mehmanmehman182@gmail.com>
2023-09-28 21:58:23 +0400

    header section

--- home/admin.py
+++ home/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import *
 # Register your models here.
+
+class SocialLinkInline(admin.StackedInline):
+    model = SocialMediaLink
+    extra = 1    
+
+@admin.register(HeaderData)
+class HeaderDataAdmin(admin.ModelAdmin):
+    model = HeaderData
+    inlines = [SocialLinkInline]
--- home/models.py
+++ home/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
 # Create your models here.
+
+class HeaderData(models.Model):        
+    email = models.EmailField()
+    location = models.CharField(max_length=250)
+
+    def __str__(self):
+        return "Header Info"
+
+
+class SocialMediaLink(models.Model):
+    class SocialMediaAccount(models.TextChoices):
+        FB = "Facebook", "Facebook"
+        TW = "Twitter", "Twitter"
+        IN = "LinkedIn", "LinkedIn"
+        IG = "Instagram", "Instagram"
+
+    header_data = models.ForeignKey(HeaderData, on_delete=models.CASCADE, related_name="social_media_links")
+    platform = models.CharField(max_length=50, choices=SocialMediaAccount.choices)
+    link = models.URLField()
+
+
+    def __str__(self):
+        return self.platform
+    
+
